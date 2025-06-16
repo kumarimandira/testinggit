15b5d7b6af86b80d5a3581cdd8a90a6eb4cd3196
kumarimandira <kumari.mandira123@gmail.com>
2025-06-16 10:31:50 +0530

    testingitrepo

---
+++ DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'my3tierdb.cn280g0ocud0.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'root123456',
+    DB_DATABASE : 'webappdb'
+});
